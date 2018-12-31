5eb736b15e8cacd822742954bdf35c59fcebf2e2
Christopher Croal <ccroal@ntlworld.com>
2018-12-31 15:11:26 +0000

    javascript created but not posting

--- js/app.js
+++ js/app.js
@@ -1,6 +1,36 @@
-document.addEventListener = ('DOMContentLoaded', () => {
+document.addEventListener('DOMContentLoaded', () => {
   console.log('JavaScript loaded');
 
 
+  const form = document.querySelector('#new-film-form')
 
-};
+
+  const handleFormSubmit = (event) => {
+    event.preventDefault();
+
+    const title = event.target.title.value;
+    const director = event.target.director.value;
+    const year = event.target.year.value;
+
+    const movies = document.querySelector('#movies');
+
+
+    const movieDetails = document.createElement('div');
+    const titlePara = document.createElement('p');
+    const directorPara = document.createElement('p');
+    const yearPara = document.createElement('p');
+
+    titlePara.textContent = title;
+    directorPara.textContent = director;
+    yearPara.textContent = year;
+
+    movieDetails.appendChild(titlePara);
+    movieDetails.appendChild(directorPara);
+    movieDetails.appendChild(yearPara);
+
+    movies.appendChild(movieDetails);
+
+    form.addEventListener('submit', handleFormSubmit);
+  }
+
+})
